1a4817f827988c81ac6ec19fb61f32b54f2a7afd
Rillieux <dave.kondris@gmail.com>
2021-01-03 17:38:53 +0100

    Initial Commit

---
+++ PhotoSelectAndCrop/PhotoSelectAndCropApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSelectAndCropApp.swift
+//  PhotoSelectAndCrop
+//
+//  Created by Dave Kondris on 03/01/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSelectAndCropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
